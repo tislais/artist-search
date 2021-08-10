50139e32c3b81b23f31f1c65ad54133fddbeec8e
Katherine Tam <katherinemtam@gmail.com>
2021-08-10 13:53:44 -0700

    ArtistsList component

---
+++ src/components/artists/ArtistsList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Artist from './Artist';
+
+const ArtistsList = () => {
+  return (
+    <>
+    <h1>Artist List</h1>
+    <Artist />
+    </>
+  )
+}
+
+export default ArtistsList;
