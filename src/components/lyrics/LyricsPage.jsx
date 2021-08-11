d633f5d0a3454f666a5f13158aac800e5f37eff3
Tis Lais <tistististis@gmail.com>
2021-08-11 10:48:03 -0700

    Pass data from recordings list to lyrics page

--- src/components/lyrics/LyricsPage.jsx
+++ src/components/lyrics/LyricsPage.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const LyricsPage = () => {
+  
+  let location = useLocation();
+  let artist = location.state.artist;
+  let title = location.state.title;
+
+  console.log(artist, title);
+
   return (
     <>
       <h1>Hello!</h1>
--- src/components/recordings/Recording.jsx
+++ src/components/recordings/Recording.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Recording = ({id, title, }) => {
+const Recording = ({id, title, artist }) => {
   return ( 
   <li key={id}>
-    <Link to={`/recording/${id}`}>
+    <Link to={{
+      pathname: `/recording/${id}`,
+      state: {
+        artist: artist,
+        title: title
+}}}>
     <h1>{title}</h1>
     </Link>
   </li>
