1268705afb8d4db938e2c67fca635f284d847703
Tis Lais <tistististis@gmail.com>
2021-08-11 14:38:04 -0700

    Add click handlers and restructure data

--- src/components/search/SearchPage.jsx
+++ src/components/search/SearchPage.jsx
@@ -8,7 +8,7 @@ import SearchForm from './SearchForm';
 const SearchPage = () => {
   const [search, setSearch] = useState('');
   const [artists, setArtists] = useState([]);
-  // const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   const handleInputChange = ({ target }) => {
     setSearch(target.value);
@@ -16,12 +16,19 @@ const SearchPage = () => {
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
-    const { artists } = await fetchArtists(search);
+    const artists = await fetchArtists(search, page);
     setArtists(artists);
-    // setLoading(false);
   }
 
-  // if(loading) return <h1>Loading...</h1>;
+  const handleNextClick = () => {
+    setPage((prevPage) => prevPage + 1)
+  };
+
+  const handlePrevClick = () => {
+    setPage((prevPage) => {
+      return prevPage - 1
+    })
+  };
 
   return (
     <>
@@ -29,7 +36,10 @@ const SearchPage = () => {
         onInputChange={handleInputChange} 
         onFormSubmit={handleFormSubmit}
         search={search} />
-      <Pagination />
+      <Pagination 
+        page={page}
+        onNextClick={handleNextClick}
+        onPrevClick={handlePrevClick} />
       <ArtistsList artists={artists} />
     </>
   )
