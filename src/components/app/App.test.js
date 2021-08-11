825208387fba698a986ab8e6b015709b2ee4f64b
Zachary Gaines <zacharygaines28@gmail.com>
2021-08-11 16:52:48 -0700

    broken test

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { list } from 'purgecss/node_modules/postcss';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('app component', () => {
+  it('displays a bunch of artists', async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    )
+    const input = screen.getByPlaceholderText('Search an Artist')
+    fireEvent.change(input, { target: { value: 'dj' } })
+    expect(input.value).toBe('dj')
+
+    // const onSubmit = jest.fn();
+    // const form = screen.getByTestId('form')
+    // fireEvent.submit(form)
+    // expect(onSubmit).toHaveBeenCalledTimes(1)
+
+    const button = screen.getByTestId('form-button')
+    const ul = await screen.findByRole('list')
+    console.log(ul)
+    userEvent.click(button)
+    expect(ul).not.toBeEmptyDOMElement()
+  })
+})
+
