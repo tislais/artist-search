8369aa181e6a18d82a042600e8bc7a710b5d9214
Katherine Tam <katherinemtam@gmail.com>
2021-08-10 18:03:40 -0700

    fetchRecordings function

--- src/services/searchApi.js
+++ src/services/searchApi.js
@@ -1,15 +1,20 @@
 export const fetchArtists = async (search) => {
   const res = await fetch(`http://musicbrainz.org/ws/2/artist?query=${search}&fmt=json&limit=25`);
-  const json = res.json();
+  const json = await res.json();
   return json;
 }
 
 export const fetchReleases = async (id) => {
-  console.log(id)
   const res = await fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json`);
   const json = await res.json();
 
-  console.log(json)
   return json.releases;
 }
 
+export const fetchRecordings = async (id) => {
+  console.log(id);
+  const res = await fetch(`http://musicbrainz.org/ws/2/recording?release=${id}&fmt=json`)
+  const json = await res.json();
+
+  return json.recordings;
+}
