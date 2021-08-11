2ce40ed053e9b07fba70622a5a9f51892b0e849c
Katherine Tam <katherinemtam@gmail.com>
2021-08-10 18:02:53 -0700

    adjust link pathing

--- src/components/artists/Artist.jsx
+++ src/components/artists/Artist.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Artist = ({ name, id, country }) => {
   return (
     <li key={id}>
-      <Link to={`/${id}/releases`}>
+      <Link to={`/artist/${id}`}>
         <h2>{name}</h2>
         <p>{country}</p>
       </Link>
--- src/components/releases/Release.jsx
+++ src/components/releases/Release.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Release = ({ id, title }) => {
 
   return (
     <li key={id}>
-      <h1>{title}</h1>
-      <img src={`http://coverartarchive.org/release/${id}/front
-`} alt={title + ' image'} />
+      <Link to={`/release/${id}`}>
+        <h1>{title}</h1>
+        <img src={`http://coverartarchive.org/release/${id}/front`} alt={title + ' image'} />
+      </Link>
     </li>
   )
 }
