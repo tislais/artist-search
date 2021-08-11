cd67ff3c08c05199362628c2c70d882ef59bd086
Katherine Tam <katherinemtam@gmail.com>
2021-08-11 13:43:54 -0700

    Lyrics render on LyricsPage

--- src/components/lyrics/LyricsPage.jsx
+++ src/components/lyrics/LyricsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { fetchSong } from '../../services/searchApi';
 
@@ -9,16 +9,25 @@ const LyricsPage = () => {
   let location = useLocation();
   let artist = location.state.artist;
   let title = location.state.title;
+  const [lyricsData, setLyricsData] = useState ({})
+  const [lyrics, setLyrics] = useState('');
+  const [coverArt, setCoverArt] = useState('');
 
-  useEffect(() => {
-    const lyrics = fetchSong(artist, title)
-    console.log(lyrics)
-
+  useEffect(async() => {
+    const lyricsData = await fetchSong(artist, title);
+    setLyrics(lyricsData.lyrics);
+    setCoverArt(lyricsData.coverArt);
+    
+    // console.log('data', lyricsData)
+    // console.log('lyrics', lyricsData.lyrics)
+    // console.log('coverArt', lyricsData.coverArt)
   }, [artist, title])
 
   return (
     <>
       <h1>Hello!</h1>
+      <img src={coverArt} alt={title} />
+      <p>{lyrics}</p>
     </>
   )
 }
